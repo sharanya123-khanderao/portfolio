8b0c3a7035fb2630a79ec502891bb9a42195e529
venkata naga sharanya khande rao <venkatanagasharanyakhanderao@venkatas-MacBook-Air-2.local>
2025-10-05 22:14:28 -0400

    fix vercel error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/ThemeContext";
@@ -7,5 +7,6 @@ import { ThemeProvider } from "@/components/ThemeContext";
 createRoot(document.getElementById("root")!).render(
 	<ThemeProvider>
 		<App />
+		<Analytics />
 	</ThemeProvider>
 );
